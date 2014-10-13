e6b35a118042dcb881df84c95024dca83f1636bb
Mauricio Del Carpio <mdelcarpio@acm.org>
2014-10-12 19:42:10 -0700

    Adding grades model

---
+++ app/models/grades_spreadsheet.rb
@@ -0,0 +1,2 @@
+class GradesSpreadsheet < ActiveRecord::Base
+end
---
+++ test/models/grades_spreadsheet_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class GradesSpreadsheetTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
