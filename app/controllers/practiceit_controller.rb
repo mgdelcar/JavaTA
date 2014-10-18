525684fa78def741892c3336f9becfbc689f784b
Mauricio Del Carpio <mdelcarpio@acm.org>
2014-10-18 16:47:26 -0700

    Improve the messages shown to the final user

--- app/controllers/practiceit_controller.rb
+++ app/controllers/practiceit_controller.rb
@@ -9,20 +9,22 @@ class PracticeitController < ApplicationController
       grades_spreadsheet = GradesSpreadsheet.new(ENV['BB_EMAIL'], ENV['BB_CRED'], params[:doc_id])
 
       students = grades_spreadsheet.students_list
+      successful_saves = 0
 
       students.each do |name, info|
         begin
           @log_messages << "Reading grades of #{name}"
           student_grades = Crawler.read_student_grades(info[:username], info[:credential])
           info[:grades] = student_grades
+          successful_saves += 1
         rescue Exception => msg
-          $stderr.puts msg
+          @error_messages << msg
         end
       end
 
       grades_spreadsheet.save_grades
 
-      @log_messages << "Student grades saved successfully"
+      @log_messages << "#{successful_saves} student grades saved successfully of #{students.count}"
 
     rescue Exception => msg
       @error_messages << msg
