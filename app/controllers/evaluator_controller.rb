62e8070ae6f217d6eb40c13661eabb1120634f90
Mauricio Del Carpio <mdelcarpio@acm.org>
2014-12-23 17:49:04 -0500

    Adding more proof of concept compilations

--- app/controllers/evaluator_controller.rb
+++ app/controllers/evaluator_controller.rb
@@ -2,15 +2,17 @@ require 'open3'
 
 class EvaluatorController < ApplicationController
   def compile
+    
+    class_name = 'Sample2'
     location = './test_code'
-    cmd = "javac #{location}/Sample1.java"
+    cmd = "javac #{location}/#{class_name}.java"
 
     Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
       @compile_output = stdout.read
       @compile_output_error = stderr.read
     end
 
-    cmd = "java -cp #{location} Sample1"
+    cmd = "java -cp #{location} #{class_name}"
 
     Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
       @output = stdout.read
---
+++ test_code/Sample2.java
@@ -0,0 +1,8 @@
+class Sample2
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello world. It works!");
+		Tryng something
+	}
+}
