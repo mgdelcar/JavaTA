acb056a9f1c81b8806e129f1cd4161457102307f
Mauricio Del Carpio <mdelcarpio@acm.org>
2015-02-01 14:31:32 -0800

    Adding back access type

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 #config/initalizers/omniauth.rb
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['CLIENT_ID'], ENV['CLIENT_SECRET'], {
-  scope: ['email']}
+  provider :google_oauth2, ENV['CLIENT_ID'], ENV['CLIENT_SECRET'],
+  { scope: ['email'], access_type: 'offline' }
 end
