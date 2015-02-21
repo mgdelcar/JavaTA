95331c9c49d8efd47ad3d6e59052b92376ac9f09
Mauricio Del Carpio <mdelcarpio@acm.org>
2015-02-20 18:32:01 -0800

    Sort users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.all.order(user_type: :desc, last_name: :desc, name: :desc)
   end
 
   # GET /users/1
